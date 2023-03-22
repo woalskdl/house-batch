437583fb2bb7a7b29c93cd58d356c9834f652119
jay <jay.wekeep@gmail.com>
2023-03-22 09:10:45 +0900

    Batch Job, Step, FlatFileItemReader

---
+++ src/main/java/com/fastcampus/housebatch/core/repository/LawdRepository.java
@@ -0,0 +1,7 @@
+package com.fastcampus.housebatch.core.repository;
+
+import com.fastcampus.housebatch.core.entity.Lawd;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LawdRepository extends JpaRepository<Lawd, Long> {
+}
