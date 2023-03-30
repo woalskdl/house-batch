9768a5a2bd761bebf8d8b84f5bb3888047f6b2d2
jay <jay.wekeep@gmail.com>
2023-03-30 09:03:20 +0900

    refactoring

--- src/main/java/com/fastcampus/housebatch/job/apt/AptDealInsertJobConfig.java
+++ src/main/java/com/fastcampus/housebatch/job/apt/AptDealInsertJobConfig.java
@@ -3,20 +3,17 @@ package com.fastcampus.housebatch.job.apt;
 import com.fastcampus.housebatch.adapter.ApartmentApiResource;
 import com.fastcampus.housebatch.core.dto.AptDealDto;
 import com.fastcampus.housebatch.core.repository.LawdRepository;
-import com.fastcampus.housebatch.job.validator.LawdCdParameterValidator;
 import com.fastcampus.housebatch.job.validator.YearMonthParameterValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.*;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.xml.StaxEventItemReader;
 import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
@@ -27,8 +24,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 import java.time.YearMonth;
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -38,7 +33,6 @@ public class AptDealInsertJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final ApartmentApiResource apartmentApiResource;
-    private final LawdRepository lawdRepository;
 
     @Bean
     public Job aptDealInsertJob(
@@ -48,26 +42,13 @@ public class AptDealInsertJobConfig {
     ) {
         return jobBuilderFactory.get("aptDealInsertJob")
                 .incrementer(new RunIdIncrementer())
-                .validator(aptDealJobParameterValidator())
+                .validator(new YearMonthParameterValidator())
                 .start(guLawdCdStep)
                 .on("CONTINUABLE").to(contextPrintStep).next(guLawdCdStep)
                 .from(guLawdCdStep)
-                .on("*" ).end()
+                .on("*").end()
                 .end()
-//                .next(contextPrintStep)
-//                .next(aptDealInsertStep)
                 .build();
-    }
-
-    private JobParametersValidator aptDealJobParameterValidator() {
-        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
-        // List 로 순서대로 진행됨.
-        validator.setValidators(Arrays.asList(
-                new YearMonthParameterValidator()
-//                new LawdCdParameterValidator()
-        ));
-
-        return validator;
     }
 
     @JobScope
@@ -78,47 +59,10 @@ public class AptDealInsertJobConfig {
                 .build();
     }
 
-    /**
-     * ExecutionContext 에 저장할 데이터
-     * 1. guLawdCd      > 구 코드 - 다음 스텝에서 활용할 값
-     * 2. guLawdCdList  > 구 코드 리스트
-     * 3. itemCount     > 남아있는 구 코드의 갯수
-     */
     @StepScope
     @Bean
-    public Tasklet guLawdCdTasklet() {
-        return (contribution, chunkContext) -> {
-            StepExecution stepExecution = chunkContext.getStepContext().getStepExecution();
-            // step to step 전달을 위해
-            ExecutionContext executionContext = stepExecution.getJobExecution().getExecutionContext();
-
-            // 데이터가 있으면 다음 스텝을 실행하고, 없으면 종료하도록 설정
-            // 데이터가 있으면 > CONTINUALBLE
-            List<String> guLawdCdList;
-            if (!executionContext.containsKey("guLawdCdList")) {
-                guLawdCdList = lawdRepository.findDistinctGuLawdCd();
-                executionContext.put("guLawdCdList", guLawdCdList);
-                executionContext.putInt("itemCount", guLawdCdList.size());
-            } else {
-                guLawdCdList = (List<String>) executionContext.get("guLawdCdList");
-            }
-
-            int itemCount = executionContext.getInt("itemCount");
-
-            if (itemCount == 0) {
-                contribution.setExitStatus(ExitStatus.COMPLETED);
-                return RepeatStatus.FINISHED;
-            }
-
-            itemCount -= 1;
-
-            String guLawdCd = guLawdCdList.get(itemCount);
-            executionContext.putString("guLawdCd", guLawdCd);
-            executionContext.putInt("itemCount", itemCount);
-
-            contribution.setExitStatus(new ExitStatus("CONTINUABLE"));
-            return RepeatStatus.FINISHED;
-        };
+    public Tasklet guLawdCdTasklet(LawdRepository lawdRepository) {
+        return new GuLawdTasklet(lawdRepository);
     }
 
     @JobScope
@@ -158,7 +102,6 @@ public class AptDealInsertJobConfig {
     @StepScope
     @Bean
     public StaxEventItemReader<AptDealDto> aptDealResourceReader(
-//            @Value("#{jobParameters['filePath']}") String filePath,
             @Value("#{jobParameters['yearMonth']}") String yearMonth,
             @Value("#{jobExecutionContext['guLawdCd']}") String guLawdCd,
             Jaxb2Marshaller aptDealDtoMarshaller
